03d6561873986a5a3435f42f1d7cba6798df8f72
DevCardinal1 <dev@cardinalco.email>
2023-08-08 20:48:39 +0530

    Designed Slider section

--- src/scripts/components/sections/CategorySlider.vue
+++ src/scripts/components/sections/CategorySlider.vue
@@ -1,174 +1,189 @@
 <template>
-    <section class="category-slider">
-      <div class="container">
-        <div class="category-slider__title-wrapper" :class="{'w-full':!isSlider}">
-          <h2 class="category-slider__title" >{{ schema.settings.title }}</h2>
-          <div class="category-slider__btn" v-if="isSlider">
-            <button class="category-slider__btn__previous-slide" @click="previousSlide">
-              <icon name="chevron-left" size="14px" />
-            </button>
-            <button class="category-slider__btn__next-slide" @click="nextSlide">
-              <icon name="chevron-right" size="14px" />
-            </button>
-          </div>
-        </div>
-        <swiper :options="desktopOptions" ref="categorySlider" v-if="isSlider">
-          <swiper-slide v-for="block in schema.blocks" :key="block.id">
-            <v-link :href="block.settings.slider_link_url" class="category-slider__slide">
-              <div class="category-slider__slide__image">
-                <img :src="block.settings.slider_image.src" :alt="block.settings.slider_title" v-if="block.settings.slider_image.src"/>
-                <img :src="block.settings.slider_image" :alt="block.settings.slider_title" v-if="block.settings.slider_image" />
-              </div>
-              <div class="category-slider__detail">
-                <p>{{ block.settings.slider_title }}</p>
-                <icon name="chevron-right" size="10px" />
-              </div>
-            </v-link>
-          </swiper-slide>
-        </swiper>
-        <div class="category-slider__slide-container" v-if="!isSlider">
-          <div class="category-slider__slide-single" v-for="block in schema.blocks" :key="block.id">
-            <v-link :href="block.settings.slider_link_url" class="category-slider__slide">
-              <div class="category-slider__slide__image">
-                <img :src="block.settings.slider_image.src" :alt="block.settings.slider_title" v-if="block.settings.slider_image.src"/>
-                <img :src="block.settings.slider_image" :alt="block.settings.slider_title" v-if="block.settings.slider_image" />
-              </div>
-              <div class="category-slider__detail">
-                <p>{{ block.settings.slider_title }}</p>
-                <icon name="chevron-right" size="10px" />
-              </div>
-            </v-link>
-          </div>
-          
+  <section class="category-slider">
+    <div class="container">
+      <div class="category-slider__title-wrapper" :class="{'w-full':!isSlider}">
+        <h2 class="category-slider__title" >{{ schema.settings.title }}</h2>
+        <div class="category-slider__btn" v-if="isSlider">
+          <button class="category-slider__btn__previous-slide" @click="previousSlide">
+            <icon name="chevron-left" size="14px" />
+          </button>
+          <button class="category-slider__btn__next-slide" @click="nextSlide">
+            <icon name="chevron-right" size="14px" />
+          </button>
         </div>
       </div>
-    </section>
-  </template>
-  <script>
-  import { swiper, swiperSlide } from 'vue-awesome-swiper'
-  import Icon from '../basic/Icon.vue';
-  import VLink from '../buttons/VLink.vue'
-  export default {
-    name: "CategorySlider",
-    components: {
-      swiper,
-      swiperSlide,
-      Icon,
-      VLink
-    },
-    props: {
-      schema: {
-        type: Object
-      }
-    },
-    data() {
-      return {
-        desktopOptions: {
-          slidesPerView: 4,
-          spaceBetween: 30,
-          loop: true,
-          breakpoints: {
-            765: {
-              spaceBetween: 15,
-              slidesPerView: 2,
-            }
-          },
+      <swiper :options="desktopOptions" ref="categorySlider" v-if="isSlider">
+        <swiper-slide v-for="block in schema.blocks" :key="block.id">
+          <v-link :href="block.settings.slider_link_url" class="category-slider__slide">
+            <div class="category-slider__slide__image" >
+              <img :src="block.settings.slider_image.src" :alt="block.settings.slider_title" v-if="block.settings.slider_image.src"/>
+              <img :src="block.settings.slider_image" :alt="block.settings.slider_title" v-if="block.settings.slider_image && typeof block.settings.slider_image == 'string'" />
+            </div>
+            <div class="category-slider__detail">
+              <p>{{ block.settings.slider_title }}</p>
+              <icon name="chevron-right" size="10px" />
+            </div>
+          </v-link>
+        </swiper-slide>
+      </swiper>
+      <div class="category-slider__slide-container" v-if="!isSlider">
+        <div class="category-slider__slide-single" v-for="block in schema.blocks" :key="block.id">
+          <v-link :href="block.settings.slider_link_url" class="category-slider__slide">
+            <div class="category-slider__slide__image">
+              <img :src="block.settings.slider_image.src" :alt="block.settings.slider_title" v-if="block.settings.slider_image.src"/>
+              <img :src="block.settings.slider_image" :alt="block.settings.slider_title" v-if="block.settings.slider_image && typeof block.settings.slider_image == 'string' " />
+            </div>
+            <div class="category-slider__detail">
+              <p>{{ block.settings.slider_title }}</p>
+              <icon name="chevron-right" size="10px" />
+            </div>
+          </v-link>
+        </div>
+        
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+import Icon from '../basic/Icon.vue';
+import VLink from '../buttons/VLink.vue'
+export default {
+  name: "CategorySlider",
+  components: {
+    swiper,
+    swiperSlide,
+    Icon,
+    VLink
+  },
+  props: {
+    schema: {
+      type: Object
+    }
+  },
+  data() {
+    return {
+      desktopOptions: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        loop: true,
+        breakpoints: {
+          765: {
+            spaceBetween: 15,
+            slidesPerView: 2,
+          }
         },
-        isSlider:this.schema.settings.enable_slider
-      };
-    },
-    methods: {
-      nextSlide() {
-        this.$refs.categorySlider.swiper.slideNext();
       },
-      previousSlide() {
-        this.$refs.categorySlider.swiper.slidePrev()();
-      }
+      isSlider:this.schema.settings.enable_slider
+    };
+  },
+  methods: {
+    nextSlide() {
+      this.$refs.categorySlider.swiper.slideNext();
     },
+    previousSlide() {
+      this.$refs.categorySlider.swiper.slidePrev()();
+    }
+  },
+}
+</script>
+<style scoped lang="scss">
+.category-slider {
+  padding: 35px 0 35px 0;
+
+  @media(max-width: 768px) {
+    padding: 20px 0 20px 0;
   }
-  </script>
-  <style scoped lang="scss">
-  .category-slider {
-    padding: 35px 0 70px 0;
+  
+  &__detail {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px 0;
+
+    p {
+      color: #000000;
+    }
+  }
+
+  &__title-wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    margin-bottom: 35px;
+
+    @media(max-width: 768px) {
+      text-align: left;
+      margin-bottom: 18px;
+    }
+    &.w-full{
+     .category-slider__title {
+      width: 100%;
+      margin: 0;
+     }
+     .category-slider__btn {
+      width: 100%;
+      margin: 0;
+     }
+    }
+   
+  }
+
+  &__btn__previous-slide {
+    padding-right: 15px;
+  }
+
+  &__btn__next-slide {
+    padding-left: 15px;
+  }
+
+  &__title {
+    width: 80%;
+    margin-left: 20%;
   
     @media(max-width: 768px) {
-      padding: 18px 0 36px 0;
+      margin-left: 0%;
     }
-  
-    &__detail {
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      padding: 10px 5px;
-  
-      p {
-        color: #000000;
-      }
-    }
-  
-    &__title-wrapper {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      text-align: center;
-      margin-bottom: 35px;
-  
-      @media(max-width: 768px) {
-        text-align: left;
-        margin-bottom: 18px;
-      }
-      &.w-full{
-       .category-slider__title{
-        width: 100%;
-        margin: 0;
-       }
-       .category-slider__btn{
-        width: 100%;
-        margin: 0;
-       }
-      }
-     
-    }
-  
-    &__btn__previous-slide {
-      padding-right: 15px;
-    }
-  
-    &__btn__next-slide {
-      padding-left: 15px;
-    }
-  
-    &__title {
-      width: 80%;
-      margin-left: 20%;
-    
-      @media(max-width: 768px) {
-        margin-left: 0%;
-      }
-    }
-  
+  }
+
     &__btn {
       width: 20%;
       display: flex;
       align-items: center;
       justify-content: end;
     }
-    .category-slider__slide-container{
+    .category-slider__slide-container {
       display: flex;
       align-items: center;
       flex-wrap: wrap;
-      flex-grow: 1;
-     flex-shrink: 0;
+      column-gap: 25px;
+      justify-content: center;
+
     .category-slider__slide-single {
       width: calc(25% - 25px);
-      margin-right: 25px;
+      margin-right: 0;
       margin-bottom: 25px;
       @media(max-width: 768px) {
         width: calc(50% - 15px);
-        margin-right: 15px;
         margin-bottom: 15px;
       }
-  }
+
     }
   }
-  </style>
+
+  .category-slider__detail,
+    .category-slider__detail p {
+      font-weight: 700;
+      font-size: 0.875rem;
+      line-height: 24px;
+      margin-bottom: 2px;
+      text-transform: uppercase;
+      margin: 0;
+    }
+}
+.icon.icon--arrow_long_right {
+  display: none !important;
+}
+
+</style>
